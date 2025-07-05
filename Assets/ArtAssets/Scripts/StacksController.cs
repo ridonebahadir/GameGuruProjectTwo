3c674fb1a46d27c5f15939d309e72d3bb84d5543
ridonebahadir <64854261+ridonebahadir@users.noreply.github.com>
2025-07-05 19:20:16 +0300

    feat : next level add

--- Assets/ArtAssets/Scripts/StacksController.cs
+++ Assets/ArtAssets/Scripts/StacksController.cs
@@ -10,9 +10,9 @@ public class StacksController : MonoBehaviour
     [SerializeField] private Stack stackPrefab;
     [SerializeField] private Stack firstStack;
     [SerializeField] private Material[] materials;
-    [SerializeField] private GameObject finishObj;
+    [SerializeField] private GameObject finishPrefab;
     [SerializeField] private int totalStackCount;
-    
+
     private bool _isGameFinished;
     private bool _isFinishPanel;
     private int _stackCount;
@@ -22,7 +22,7 @@ public class StacksController : MonoBehaviour
     private Stack _lastStack;
     private float _stackZ;
     private readonly float _stackLength = 2.66f;
-
+    private GameObject _finishInstance;
 
     public Action OnLoseGame;
     public Action OnWinGame;
@@ -32,7 +32,6 @@ public class StacksController : MonoBehaviour
     {
         _characterController = characterController;
     }
-
 
     private void Start()
     {
@@ -45,12 +44,12 @@ public class StacksController : MonoBehaviour
 
         _stackZ = _lastStack.transform.position.z;
         SpawnStack();
-        
+
         var finishZ = _stackZ + (_stackLength * totalStackCount);
-        var finishPos = new Vector3(0f, 0.5f, finishZ); 
-        
-        finishObj.transform.position = finishPos;
+        var finishPos = new Vector3(0f, 0.5f, finishZ);
+        _finishInstance = Instantiate(finishPrefab, finishPos, Quaternion.identity);
     }
+
 
     private void Update()
     {
@@ -132,32 +131,59 @@ public class StacksController : MonoBehaviour
         _isGameFinished = true;
     }
 
-
     private void JumpCharacter()
     {
         OnWinGame?.Invoke();
-        
+
+        if (_finishInstance == null) return;
+
         var targetJump = new Vector3(
-            finishObj.transform.position.x,
+            _finishInstance.transform.position.x,
             _characterController.transform.position.y,
-            finishObj.transform.position.z
+            _finishInstance.transform.position.z
         );
 
         _characterController.JumpTo(targetJump);
     }
-    
+
     public void NextLevel()
     {
-        _stackCount = 0;
         _isGameFinished = false;
         _isFinishPanel = false;
-        _stackZ = _characterController.transform.position.z;
-        _lastStack = null;
-        var finishZ = _stackZ + (_stackLength * totalStackCount);
-        var finishPos = new Vector3(0f, 0.5f, finishZ);
-        finishObj.transform.position = finishPos;
-        finishObj.SetActive(true);
+        _stackCount = 0;
+        
+        var newStackZ = _finishInstance.transform.position.z + _stackLength;
+        var newStackPos = new Vector3(0f, 0f, newStackZ);
+
+        firstStack = Instantiate(stackPrefab, newStackPos, Quaternion.identity, transform);
+        firstStack.Initialize(Vector3.zero, materials[Random.Range(0, materials.Length)]);
+        firstStack.IsMoving = false;
+
+        _lastStack = firstStack;
+
+      
+        _stackZ = newStackZ + (_stackLength * totalStackCount);
+        var newFinishPos = new Vector3(0f, 0.5f, _stackZ+ _stackLength);
+        _finishInstance = Instantiate(finishPrefab, newFinishPos, Quaternion.identity);
+
+      
+        var charStartPos = new Vector3(
+            firstStack.transform.position.x,
+            _characterController.transform.position.y,
+            firstStack.transform.position.z
+        );
+        _characterController.MoveTo(charStartPos);
+
+       
+        _stackZ = firstStack.transform.position.z;
+
         SpawnStack();
     }
+
+
+
+
+
+
 
 }
